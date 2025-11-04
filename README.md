44e55bc5847ad72ecd12c63fb696c0f48091e950
chengyansong.cys <chengyansong.cys@alipay.com>
2025-11-04 19:20:57 +0800

    feat: initialize project framework

---
+++ README.md
@@ -0,0 +1,5 @@
+项目介绍
+基于C++的HTTP服务框架
+
+学习笔记
+链接https://www.yuque.com/chengyansong/rq6w34
---
+++ WebApps/GomokuServer/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, HTTP Server Framework!" << std::endl;
+    return 0;
+}
