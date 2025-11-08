c9b8f1382367c6d54cc55db56feadeca4555932e
chengyansong.cys <chengyansong.cys@alipay.com>
2025-11-08 17:32:10 +0800

    feat:实现HttpRequest类(HTTP请求封装)、HttpResponse类(HTTP响应封装)和HttpContext类(HTTP请求解析上下文) HttpRequest: 封装HTTP请求数据,负责存储和管理请求方法、路径、查询参数、请求头和请求体内容 HttpResponse: 封装HTTP响应数据,负责管理响应状态码、响应头和响应体内容,提供响应格式化方法 HttpContext: 使用状态机模式解析HTTP请求报文,负责从网络缓冲区解析请求并将结果提取到HttpRequest对象中

---
+++ HttpServer/src/http/HttpResponse.cpp
@@ -0,0 +1,45 @@
+#include "../../include/http/HttpResponse.h"
+
+namespace http
+{
+    void HttpResponse::appendToBuffer(muduo::net::Buffer* outputBuf) const
+    {
+        // HttpResponse封装的信息格式化输出
+        char buf[32];
+        // 为什么不把状态信息放入格式化字符串中，因为状态信息有长有短，不方便定义一个固定大小的内存存储
+        snprintf(buf, sizeof(buf), "%s %d ", httpVersion_.c_str(), statusCode_);
+
+        outputBuf->append(buf);
+        outputBuf->append(statusMessage_);
+        outputBuf->append("\r\n");
+
+        if (closeConnection_)
+        {
+            outputBuf->append("Connection: close\r\n");
+        }
+        else
+        {
+            outputBuf->append("Connection: Keep-Alive\r\n");
+        }
+
+        for (const auto& header : headers_)
+        {
+            outputBuf->append(header.first);
+            outputBuf->append(": ");
+            outputBuf->append(header.second);
+            outputBuf->append("\r\n");
+        }
+
+        outputBuf->append("\r\n");
+        outputBuf->append(body_);
+    }
+
+    void HttpResponse::setStatusLine(const std::string& version,
+        HttpStatusCode statusCode,
+        const std::string& statusMessage)
+    {
+        httpVersion_ = version;
+        statusCode_ = statusCode;
+        statusMessage_ = statusMessage;
+    }
+}
